5647216c9556616b91d4acc51261bb11cd3d12e8
jinLi <1225258449@qq.com>
2020-07-13 22:49:31 +0800

    1

---
+++ 01/src/main/java/Student.java
@@ -0,0 +1,11 @@
+/**
+ * Package: PACKAGE_NAME
+ *
+ * @author : Lijin
+ * @date :  2020/7/13
+ */
+public class Student {
+	private String cname;
+	private Integer cid;
+
+}
--- 01/src/main/java/User.java
+++ 01/src/main/java/User.java
@@ -8,7 +8,12 @@ public class User {
 
 	private Integer id;
 	private String name;
+	private Integer age;
 
+	public User(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 	public Integer getId() {
 		return id;
